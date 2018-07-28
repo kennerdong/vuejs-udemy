1164a33bc7d389040236e92a8b70b61dc2f3726b
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-29 01:52:56 +0800

    completed section 8 item 111

---
+++ section-8/src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User, {{name}}!</p>
+        <button @click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :username="name"
+                @usernameReset="name=$event"
+                :resetNameFn="resetNameFn"
+                ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return{
+                name: 'Ken'
+                // name: undefined
+            }
+        },
+        methods: {
+            changeName(){
+                this.name = 'Max'
+            },
+            resetNameFn(){
+                this.name = "Ken"
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ section-8/src/components/UserDetail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName()}}</p>
+        <button @click="resetName">Reset Name To Anne</button>
+        <button @click="resetNameFn">Reset Name To Ken</button>
+    </div>
+</template>
+
+<script>
+export default {
+	props: {
+		username: {
+            String,
+            // required: true,
+            default: 'Anonymous'
+        },
+        resetNameFn: Function
+	},
+	methods: {
+		switchName() {
+			return this.username
+				.split('')
+				.reverse()
+				.join('')
+        },
+        resetName(){
+            this.username = "Anne";
+            this.$emit('usernameReset', this.username);
+        }
+	}
+}
+</script>
+
+<style scoped>
+div {
+	background-color: lightcoral;
+}
+</style>
