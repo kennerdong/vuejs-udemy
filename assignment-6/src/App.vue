cbbf11add72e56ba7242a40260d7d0e72a9563c4
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-29 00:17:42 +0800

    completed assignment 6 & refactored folder structure

--- assignment-6/src/App.vue
+++ assignment-6/src/App.vue
@@ -1,19 +1,18 @@
 <template>
     <div class="container">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-xs-12">
                 <header>
                     <h1>Server Status</h1>
                 </header>
             </div>
-        </div>
+        </div> -->
+        <Header></Header>
         <hr>
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
+                    <li class="list-group-item" v-for="index in 5">
                         Server #{{ index }}
                     </li>
                 </ul>
@@ -21,15 +20,17 @@
             <div class="col-xs-12 col-sm-6">
                 <p>Server Details are currently not updated</p>
             </div>
-        </div>
+        </div> -->
+        <ServerContainer></ServerContainer>
         <hr>
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-xs-12">
                 <footer>
                     <p>All Servers are managed here</p>
                 </footer>
             </div>
-        </div>
+        </div> -->
+        <Footer></Footer>
     </div>
 </template>
 
@@ -37,5 +38,4 @@
 </script>
 
 <style>
-
 </style>
--- assignment-6/src/main.js
+++ assignment-6/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import ServerContainer from './components/ServerContainer.vue'
+
+Vue.component('Header', Header);
+Vue.component('Footer', Footer);
+Vue.component('ServerContainer', ServerContainer);
 
 new Vue({
   el: '#app',
