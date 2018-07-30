c7e323ab519c4c09e8398e6cfdc6106c635641e3
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-30 18:10:19 +0800

    completed section 9 lesson 123

---
+++ section-9/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <appQuote>
+                    <!-- <h2>Another Wonderful Quote!!!!</h2> -->
+                    <h2 slot="title">{{quoteTitle}}</h2>
+                    <q slot="content">Thou shall not kill</q>
+                </appQuote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+export default {
+    data: function(){
+        return({
+            quoteTitle: 'Another Wonderful Quote'
+        })
+    },
+	components: {
+		appQuote: Quote
+	}
+}
+</script>
+
+<style scoped>
+    h2{
+        color:blue;
+    }
+</style>
