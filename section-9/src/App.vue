a571d1e75f4d074a304509d4013506f86a02e422
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-30 18:54:50 +0800

    Completed Section 9 Lesson 129

--- section-9/src/App.vue
+++ section-9/src/App.vue
@@ -2,11 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <appQuote>
-                    <!-- <h2>Another Wonderful Quote!!!!</h2> -->
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+                <!-- <appQuote>
                     <h2 slot="title">{{quoteTitle}}</h2>
-                    <q slot="content">Thou shall not kill</q>
-                </appQuote>
+                    <p slot="subtitle">Passed in subtitle</p>
+                    <q>Thou shall not kill</q>
+                </appQuote> -->
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Component</p>
+                    </component>
+                </keep-alive>
             </div>
         </div>
     </div>
@@ -14,20 +24,26 @@
 
 <script>
 import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import New from './components/New.vue'
+
 export default {
-    data: function(){
-        return({
-            quoteTitle: 'Another Wonderful Quote'
-        })
-    },
+	data: function() {
+		return {
+			quoteTitle: 'Another Wonderful Quote',
+			selectedComponent: 'appQuote'
+		}
+	},
 	components: {
-		appQuote: Quote
+		appQuote: Quote,
+		appAuthor: Author,
+		appNew: New
 	}
 }
 </script>
 
 <style scoped>
-    h2{
-        color:blue;
-    }
+h2 {
+	color: blue;
+}
 </style>
---
+++ section-9/src/components/New.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Add Me!</button>
+    <p>Counts: {{counter}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      counter: 0
+    }
+  },
+  destroyed(){
+    // alert("Destroyed!");
+  }
+}
+</script>
+
+<style scoped>
+</style>
