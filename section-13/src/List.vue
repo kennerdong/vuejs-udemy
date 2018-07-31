6548131fffe48bf070d635ac25348c55104338d8
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-08-01 03:03:54 +0800

    section 13 lesson 173

---
+++ section-13/src/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <input type="text" v-model="filteredText">
+    <ul>
+      <li v-for="fruit in filteredFruits">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			text: 'Hello World!',
+			fruits: ['apple', 'orange', 'mango', 'durian'],
+			filteredText: ''
+		}
+	},
+	computed: {
+		filteredFruits() {
+			return this.fruits.filter(element => {
+				return element.match(this.filteredText)
+			})
+		}
+	}
+}
+</script>
+
+<style>
+</style>
