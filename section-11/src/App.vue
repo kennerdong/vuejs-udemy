dac436da1d18b01bfb225e3506816c4d984a9818
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-31 20:40:05 +0800

    completed sction 11 lesson 154

--- section-11/src/App.vue
+++ section-11/src/App.vue
@@ -60,16 +60,23 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button class="btn btn-primary">Submit!
+                    <button class="btn btn-primary"
+                    @click.prevent="submitForm"
+                    >Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -88,7 +95,7 @@
                         </ul>
                         <p>Gender: {{gender}}</p>
                         <p>Priority: {{selectedPriority}}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -97,6 +104,7 @@
 </template>
 
 <script>
+import Switch from './Switch.vue'
 export default {
 	data() {
 		return {
@@ -109,9 +117,19 @@ export default {
 			sendMail: [],
 			gender: 'Male',
 			priorities: ['High', 'Medium', 'Low'],
-			selectedPriority: 'High'
+            selectedPriority: 'High',
+            dataSwitch: true,
+            isSubmitted: false
 		}
-	}
+    },
+    methods: {
+        submitForm(){
+            this.isSubmitted=true;
+        }
+    },
+    components: {
+        appSwitch: Switch
+    }
 }
 </script>
 
