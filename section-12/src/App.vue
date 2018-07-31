5539ec689f0696b1dbea7949a657506a5430892a
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-31 23:57:32 +0800

    completed section 12 lesson 167

--- section-12/src/App.vue
+++ section-12/src/App.vue
@@ -13,6 +13,8 @@
                 <p v-highlight:background="'red'">Color this</p>
                 <p v-highlight.delayed="'red'">Color this in 3 seconds</p>
                 <p v-local-highlight.delayed="'red'">Color this local highlight in 1 second</p>
+                <p v-local-highlight.delayed.blink="'yellow'">Color this local highlight with blink after 3 seconds</p>
+                <p v-local-highlight.delayed.blink="{mainColor:'yellow', secondColor:'green', delay: 500}">Color this local highlight with blink after 3 seconds</p>
             </div>
         </div>
     </div>
@@ -25,15 +27,33 @@ export default {
 			bind(el, binding, vnode) {
 				var delay = 0
 				if (binding.modifiers['delayed']) {
-					delay = 1000;
+					delay = 1000
 				}
-				setTimeout(() => {
-					if (binding.arg == 'background') {
-						el.style.backgroundColor = binding.value
-					} else {
-						el.style.color = binding.value
-					}
-				}, delay)
+				if (binding.modifiers['blink']) {
+					let mainColor = binding.value.mainColor;
+					let secondColor = binding.value.secondColor;
+					let currentColor = mainColor
+					setTimeout(() => {
+						setInterval(() => {
+							currentColor == secondColor
+								? (currentColor = mainColor)
+								: (currentColor = secondColor)
+							if (binding.arg == 'background') {
+								el.style.backgroundColor = currentColor
+							} else {
+								el.style.color = currentColor
+							}
+						}, binding.value.delay)
+					}, delay)
+				} else {
+					setTimeout(() => {
+						if (binding.arg == 'background') {
+							el.style.backgroundColor = binding.value.mainColor
+						} else {
+							el.style.color = binding.value
+						}
+					}, delay)
+				}
 			}
 		}
 	}
