9c5b6bc619f34ff61f35c16751ffadfba52589f2
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-08-02 02:07:31 +0800

    section 14 lesson 199. Added package.json for codesandbox.io website

--- section-14/src/App.vue
+++ section-14/src/App.vue
@@ -4,19 +4,44 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br><br>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
-                <transition name="fade">
-                    <div class="alert alert-info" v-if="show">Fade</div>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-show="show">This is some Info</div>
                 </transition>
-                <transition name="slide">
-                    <div class="alert alert-info" v-if="show">Slide</div>
+                <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
-                <transition name="slide-fade">
-                    <div class="alert alert-info" v-if="show">Slide + Fade</div>
+                <transition
+                        enter-active-class="animated bounce"
+                        leave-active-class="animated shake"
+                >
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
-                <transition name="slide-fade" appear>
-                    <div class="alert alert-info">Animate Initial Loading using 'appear' keyword</div>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+                </transition>
+				<hr>
+				<button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                        @before-enter="beforeEnter"
+                        @enter="enter"
+                        @after-enter="afterEnter"
+                        @enter-cancelled="enterCancelled"
+
+                        @before-leave="beforeLeave"
+                        @leave="leave"
+                        @after-leave="afterLeave"
+                        @leave-cancelled="leaveCancelled"
+                        :css="false">
+                    <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
                 </transition>
             </div>
         </div>
@@ -27,8 +52,60 @@
 export default {
 	data() {
 		return {
-			show: false
+			show: false,
+                load: true,
+                alertAnimation: 'fade',
+                elementWidth: 100,
 		}
+	},
+	methods: {
+		beforeEnter(el){
+			console.log('beforeEnter');
+			this.elementWidth = 100;
+			el.style.width = this.elementWidth + 'px';
+		},
+		enter(el,done){
+			console.log('enter');
+			let round = 1;
+			const interval = setInterval(()=>{
+				el.style.width = (this.elementWidth + round * 10) + 'px';
+				round++;
+				if(round >20){
+					clearInterval(interval);
+					done();
+				}
+			}, 20)
+			done();
+		},
+		afterEnter(el){
+			console.log('afterEnter');
+		},
+		enterCancelled(el){
+			console.log('enterCancelled');
+		},
+		beforeLeave(el){
+			console.log('beforeLeave');
+			this.elementWidth = 300;
+			el.style.width = this.elementWidth + 'px';
+		},
+		leave(el,done){
+			console.log('enter');
+			let round = 1;
+			const interval = setInterval(()=>{
+				el.style.width = (this.elementWidth - round * 10) + 'px';
+				round++;
+				if(round >20){
+					clearInterval(interval);
+					done();
+				}
+			}, 20)
+		},
+		afterLeave(el){
+			console.log('afterLeave');
+		},
+		leaveCancelled(el){
+			console.log('leaveCancelled');
+		},
 	}
 }
 </script>
