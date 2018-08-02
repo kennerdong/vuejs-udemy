ef941e721baf588052d1a582cea43b487cdb3a03
Kenneth Ong <xenon830@hotmail.com>
2018-08-02 18:55:49 +0800

    Section 15 lesson 214

--- section-15/src/App.vue
+++ section-15/src/App.vue
@@ -3,6 +3,21 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                  <label for="">Username</label>
+                  <input class="form-control" type="text" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label for="">Email</label>
+                  <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="u in users">{{u.username}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -10,6 +25,39 @@
 
 <script>
     export default {
+      data(){
+        return{
+          user:{
+            username: "",
+            email: ""
+          },
+          users: []
+        }
+      },
+      methods: {
+        submit(){
+          this.$http.post(
+            'https://vuejs-udemy-course-8712f.firebaseio.com/data.json',
+            this.user)
+            .then(response=>{
+              console.log(response);
+            }, error=>{console.log(error)}
+            );
+        },
+        fetchData(){
+          this.$http.get('https://vuejs-udemy-course-8712f.firebaseio.com/data.json')
+          .then(response=>{
+            return response.json();
+          })
+          .then(data=>{
+            const resultArray = [];
+            for (let key in data){
+              resultArray.push(data[key]);
+            }
+            this.users = resultArray;
+          });
+        }
+      }
     }
 </script>
 
