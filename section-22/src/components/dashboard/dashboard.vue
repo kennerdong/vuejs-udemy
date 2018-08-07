f205d10168e05fa97ef26214f02491d6c466e189
Kenneth Ong <xenon830@hotmail.com>
2018-08-07 17:18:36 +0800

    Completed section 22

--- section-22/src/components/dashboard/dashboard.vue
+++ section-22/src/components/dashboard/dashboard.vue
@@ -2,8 +2,38 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{email}}</p> 
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return{
+      email: ''
+    }
+  },
+  created(){
+    axios.get('/users.json')
+    .then(response=>{
+      console.log(response);
+      const data = response.data;
+      const users = [];
+      Object.keys(data).map((e)=>{
+        const user = data[e];
+        user.id = e;
+        users.push(user);
+        this.email = users[0].email;
+        console.log(users);
+      });
+    })
+    .catch(error=>console.log(error));
+  }
+}
+</script>
+
 
 <style scoped>
   h1, p {
--- section-22/src/main.js
+++ section-22/src/main.js
@@ -1,12 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import router from './router'
-import store from './store'
+import router from "./router";
+import store from "./store";
+
+import axios from "axios";
+
+axios.defaults.baseURL = "https://vuejs-udemy-course-8712f.firebaseio.com";
+axios.defaults.headers.common["Authorization"] = "";
+axios.defaults.headers.get["Accepts"] = "application/json";
+
+axios.interceptors.request.use(config => {
+  console.log(config);
+  return config;
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   render: h => h(App)
-})
+});
